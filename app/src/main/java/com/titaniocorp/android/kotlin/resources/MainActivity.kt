377710276953bfced47fe9bab98da09f5b8a3200
Juan D. Ortiz <juanortiz@periferia-it.com>
2019-11-10 00:02:48 -0500

    - Initial Commit Project

---
+++ app/src/main/java/com/titaniocorp/android/kotlin/resources/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.titaniocorp.android.kotlin.resources
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ utilities/src/main/java/com/titaniocorp/android/kotlin/utilities/ExampleModel.kt
@@ -0,0 +1,4 @@
+package com.titaniocorp.android.kotlin.utilities
+
+class ExampleModel {
+}
